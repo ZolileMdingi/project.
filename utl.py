87ace45278d4710fa5011218a7e70885325e10e9
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-08-27 22:41:43 +0200

    Update utl.py

--- utl.py
+++ utl.py
@@ -1,5 +1,5 @@
 
-from ast import Str
+
 from bs4 import BeautifulSoup 
 import requests
 from links import Links
@@ -32,8 +32,10 @@ class Media:
 class Page:
     def __init__(self, url:URL):
         self.url = url
-        self.soup = BeautifulSoup(getData(url.getUrl()),'html.parser')
-    def getData(url:str): 
+        data = self.getHTMLData(url.getUrl())
+        self.soup = BeautifulSoup(data,'html.parser')
+        self.dict={}
+    def getHTMLData(self,url:str): 
         r = requests.get(url) #opens link
         return r.text #return html for link's webpage
     def download(self,fileName):   
@@ -43,109 +45,92 @@ class Page:
     def getData(self):
         return self.soup
     
+    def genName(self,fname):
+        if self.url.getUrl() in self.dict: #if name for local html for link has been generated
+            return self.dict[self.url.getUrl()]
+        # name = getName(self.url.getUrl()) #generate name
+        self.dict[self.url.getUrl()] =fname #add to global dictionary
+        return fname
+    def removeOnlineReferences(self,tag):
+        attrs_to_delete =[]
+        for attr in tag.attrs:
+            if 'http' in tag[attr]:
+                attrs_to_delete.append(attr)
+        for attr in attrs_to_delete:
+            del tag[attr]
+    def getDict(self):
+        return self.dict
+    def getUrl(self):
+        return self.url.getUrl()
+    def getLocation(self,link_address:str):
+        return link_address.split('?')[0]
+    def generateFilename(self,url:str):
+        return url.split('/')[-1]
+    def localiseMedia(self):
+        for tag in self.soup.find_all('img'):
+            if tag.get('src'):
+                media = Media(tag['src'], tag.name)
+                if not os.path.isdir(str(media.getDirectory())):
+                    os.makedirs(media.getDirectory())
+                filename = self.generateFilename(self.getLocation(media.getURL()))
+                filepath = media.getDirectory() +'/' +filename
+                #print(tag['src'])
+                # downloadMedia(filepath,media)
+                media.download(filepath)
+                tag['src'] = filepath
+            self.removeOnlineReferences(tag)
     
 
-site_url = URL("https://skyline3567870.wordpress.com/")
-dict={} #dictionary for all links and respective local file names
-def getLocation(link_address:str):
-    return link_address.split('?')[0]
-def generateFilename(url:str):
-    return url.split('/')[-1]
-def getData(url:str): 
-    r = requests.get(url) #opens link
-    return r.text #return html for link's webpage
+class Website:
+    def __init__(self,url:str):
+        self.site_url = URL(url)
+    def getName(self,link):
+        length = len(self.site_url.getUrl())
+        link_url = link[length:]
+        link_url = link_url.split('/')
+        if link_url[0]=='':
+            link_url[0]="home"
+        file_name = "_".join(link_url)+'.html'
+        return file_name
+    def localiseLinks(self,tagsForLinks,page:Page): #function to make links refer to local '.html' files
+        for tag in tagsForLinks: #all tags in current webpage
+            newPage = Page(URL(tag['href']))
+            # print(newPage.getUrl())
+            if newPage.getUrl() in page.getDict(): #if url has name generated already
+                tag['href'] =page.getDict()[newPage.getUrl()] #then localise it
+            else:
+                page.dict[newPage.getUrl()] = newPage.genName(self.getName(newPage.getUrl())) #generate local file name
+                tag['href'] = page.getDict()[newPage.getUrl()]#localise
+    def isSiteLink(self,url:str):
+        if len(url)>20:
+            if self.site_url.getUrl() in str(url): #check if url is wordpress bundled site
+                return True
+        return False
+    def traverse(self,url:URL,links_object):
+        if not links_object.checkExists(url.getUrl()) : #checks if url in the set of encountered links
+            links_object.addLink(url.getUrl()) #adds link url to encountered urls
+            # pageData = getData(url.getUrl())
+            # soup_object = generateSoup(pageData) #create BeautifulSoup object
+            page = Page(url)
+            listOfUrls = [] #list of all links in page
+            listoftags =[]
+            # name = genName(url.getUrl())
+            
+            for tag in tqdm(page.getData().find_all('a')):
+                if tag.get('href'):
+                    if tag.get('href')[0:4]=="http" and self.isSiteLink(tag.get('href')): #if http link and is user website link not wordpress bundled link
+                        listOfUrls.append(URL(tag.get('href')))
+                        listoftags.append(tag)
+            for link in listOfUrls: #access all links found in page while depth is less that depth    
+                self.traverse(link,links_object)#recursively download webpage for that file
+                    #link = filename
+            self.localiseLinks(listoftags,page)
+            page.localiseMedia()
+            fname = self.getName(page.getUrl())
+            page.download(page.genName(fname))
 
-def localiseLinks(tagsForLinks): #function to make links refer to local '.html' files
-    for tag in tagsForLinks: #all tags in current webpage
-        if tag['href'] in dict: #if url has name generated already
-            tag['href'] = dict[tag['href']] #then localise it
-        else:
-            dict[tag['href']] = genName(tag['href']) #generate local file name
-            tag['href'] = dict[tag['href']]#localise 
-def removeOnlineReferences(tag):
-    attrs_to_delete =[]
-    for attr in tag.attrs:
-        if 'http' in tag[attr]:
-            attrs_to_delete.append(attr)
-    for attr in attrs_to_delete:
-        del tag[attr]
-def localiseMedia(soup):
-    for tag in soup.find_all('img'):
-        if tag.get('src'):
-            media = Media(tag['src'], tag.name)
-            if not os.path.isdir(str(media.getDirectory())):
-                os.makedirs(media.getDirectory())
-            filename = generateFilename(getLocation(media.getURL()))
-            filepath = media.getDirectory() +'/' +filename
-            #print(tag['src'])
-            # downloadMedia(filepath,media)
-            media.download(filepath)
-            tag['src'] = filepath
-        removeOnlineReferences(tag)
-
-def download(data,fileName):   
-    file = open(fileName,"w") #open file to write  code to
-    file.write(data.prettify()) #write code to file
-    file.close() #save file
-
-def generateSoup(pageData):
-    return BeautifulSoup(pageData,'html.parser') #returns beautiful soup instance
-
-def isSiteLink(url:str):
-    if len(url)>20:
-        if site_url.getUrl() in str(url): #check if url is wordpress bundled site
-            return True
-    return False
-
-def traverse(url:URL,links_object):
-    if not links_object.checkExists(url.getUrl()) : #checks if url in the set of encountered links
-        links_object.addLink(url.getUrl()) #adds link url to encountered urls
-        # pageData = getData(url.getUrl())
-        # soup_object = generateSoup(pageData) #create BeautifulSoup object
-        page = Page(url)
-        listOfUrls = [] #list of all links in page
-        listoftags =[]
-        name = genName(url.getUrl())
-        for tag in tqdm(page.getData().find_all('a')):
-            if tag.get('href'):
-                if tag.get('href')[0:4]=="http" and isSiteLink(tag.get('href')): #if http link and is user website link not wordpress bundled link
-                    listOfUrls.append(URL(tag.get('href')))
-                    listoftags.append(tag)
-        for link in listOfUrls: #access all links found in page while depth is less that depth    
-            traverse(link,links_object)#recursively download webpage for that file
-                #link = filename
-        localiseLinks(listoftags)
-        localiseMedia(page.getData())
-        page.download(name)
-        # download(page.getData(),name)
-
-def genName(url):
-    if url in dict: #if name for local html for link has been generated
-        return dict[url]
-    name = getName(url) #generate name
-    dict[url] =name #add to global dictionary
-    return name
-# This function returns the rightful name that we going to use to name the file that contains
-# all the content from the downloaded link
-# it takes a single link address and returns the appropriate name
-def getName(link):
-    length = len(site_url.getUrl())
-    link_url = link[length:]
-    link_url = link_url.split('/')
-    if link_url[0]=='':
-        link_url[0]="home"
-    file_name = "_".join(link_url)+'.html'
-    return file_name
 
 the_links = Links()
+site = Website("https://skyline3567870.wordpress.com/")
+site.traverse(site.site_url,the_links)
 
-traverse(site_url,the_links)
-
-# def main():
-#     the_links = Links()
-#     site_url = "https://skyline3567870.wordpress.com/"
-#     traverse(site_url,the_links)
-
-
-# if __name__ == "__main__":
-#     main()
